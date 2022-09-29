d3aeda7fd40ea7ea3f57eac4a47aa06f11ea661f
Marc Andreu <marcandreuf@gmail.com>
2022-09-29 12:40:31 +0900

    Implementeed all methods of circular max sized queue using TDD.

--- queues/circular_sized_queue.py
+++ queues/circular_sized_queue.py
@@ -2,39 +2,56 @@ class CircQueue:
     def __init__(self, size):
         self.items = [None] * size
         self.maxSize = size
-        self.start, self.top = -1, -1
+        self.start, self.top = None, None
 
     def __str__(self):
         values = [str(x) for x in self.items]
         return f"s:{self.start} t:{self.top} {':'.join(values)}"
 
     def isEmpty(self):
-        return self.start == -1
+        return self.top == None
 
-    def _is_full(self):
+    def isFull(self):
         return self.top+1 == self.start or \
             (self.start == 0 and self.top+1 == self.maxSize)
-    
-    def _is_empty(self):
-        return self.top == -1
 
     def enqueue(self, value):
-        if self._is_empty():
+        if self.isEmpty():
             self.start = 0
             self.top = 0
-        elif self._is_full():
+        elif self.isFull():
             raise Exception('Queue is full')
-        elif self.top < self.maxSize-1:
-            self.top += 1
-        else:
+        elif self.top+1 == self.maxSize:
             self.top = 0
-        print(f"insert item at {self.top}")
+        else:
+            self.top += 1
+        #print(f"insert item at {self.top}")
         self.items[self.top]=value
        
     def dequeue(self):
+        if self.start == None:
+            return None
+        start = self.start
         ret = self.items[self.start]
-        self.start +=1
+        if self.start+1 == self.maxSize:
+            self.start = 0
+        elif self.start == self.top:
+            self.start = None
+            self.top = None
+        else:
+            self.start +=1
+        self.items[start] = None
         return ret
+
+    def peek(self):
+        return self.items[self.start]
+
+    def delete(self):
+        #for i in range(self.maxSize):
+        #    self.items[i] = None
+        self.items = [None] * self.maxSize
+        self.start = None
+        self.top = None
         
 def test_empty_circ_queue():
     cq = CircQueue(3)
@@ -93,22 +110,74 @@ def test_dequeue_from_full():
     print("test_dequeue_from_full PASS")
 
 
-def test_enqueue_after_full_dequeue():
+def test_enqueue_after_dequeue_from_full():
     cq = _sample_queue()
     cq.enqueue(3)
-    cq.dequeue()
-    print(cq)
+    e = cq.dequeue()
+    #print(cq)
+    assert e == 1
     cq.enqueue(4)
-    print(cq)
+    #print(cq)
     assert cq.start == 1
     assert cq.top == 0
-    print("test_enqueue_after_full_dequeue PASS")
+    
+    e = cq.dequeue()
+    assert e == 2
+    cq.enqueue(5)
+    #print(cq)
+    assert cq.start == 2
+    assert cq.top == 1
 
+    e = cq.dequeue()
+    assert e == 3
+    cq.enqueue(6)
+    #print(cq)
+    assert cq.start == 0
+    assert cq.top == 2
+    print("test_enqueue_after_dequeue_from_full PASS")
 
+def test_dequeu_from_empty():
+    cq = CircQueue(3)
+    e = cq.dequeue()
+    #print(cq)
+    #print(e)
+    assert e == None
+    print("test_dequeu_from_empty PASS")
+
+def test_dequeu_last_itme():
+    cq = CircQueue(3)
+    cq.enqueue(1)
+    e = cq.dequeue()
+    #print(cq)
+    assert e == 1
+    assert cq.start == None
+    assert cq.top == None
+    assert f"{cq}" == "s:None t:None None:None:None"
+    print("test_dequeu_last_itme PASS")
+
+def test_peek():
+    cq = _sample_queue()
+    e = cq.peek()
+    assert e == 1
+    assert f"{cq}" == "s:0 t:1 1:2:None"
+    print("test_peek PASS")
+
+def test_delete():
+    cq = _sample_queue()
+    cq.enqueue(4)
+    assert f"{cq}" == "s:0 t:2 1:2:4"
+    cq.delete()
+    #print(cq)
+    assert f"{cq}" == "s:None t:None None:None:None"
+    print("test_delete PASS")
 
 test_empty_circ_queue()
 test_enqueue_to_empty()
 test_enqueue_to_not_full()
 test_enqueu_to_full()
 test_dequeue_from_full()
-test_enqueue_after_full_dequeue()
+test_enqueue_after_dequeue_from_full()
+test_dequeu_from_empty()
+test_dequeu_last_itme()
+test_peek()
+test_delete()
