4e6c45fa26db4092a2cc6175069617bbf0399378
Marc Andreu <marcandreuf@gmail.com>
2022-10-12 18:10:42 +0900

    Implemented create a tree and add nodes methods with TDD.

--- trees/simple_tree.py
+++ trees/simple_tree.py
@@ -1,20 +1,20 @@
 class TreeNode:
-    def __init__(self, data, children = []):
+    def __init__(self, data, children = None):
         self.data = data
-        self.children = children
+        self.children = children if children else [] 
         
     def __str__(self, level=0):
         if level == 0:
             arrow = ""
-        elif level == 1:
-            arrow = "|_"
         else:
-            arrow = " " * (level) + "|_"
+            arrow = "  " * (level-1) + "|_"
         ret = arrow + str(self.data) + "\n"
         for child in self.children:
             ret += child.__str__(level + 1)
         return ret
 
+    def addChild(self, child):
+        self.children.append(child)
 
 def test_create_tree():
     child3 = TreeNode('c3')
@@ -22,5 +22,23 @@ def test_create_tree():
     child2 = TreeNode('c2')
     node = TreeNode('r', [child1, child2])    
     print(node)
+    assert f"{node}" == "r\n|_c1\n  |_c3\n|_c2\n"
+    print("test_create_tree PASS")
+
+
+def test_append_node():
+    child1 = TreeNode('c1')
+    node = TreeNode('r')
+    node.addChild(child1)
+    assert f"{node}" == "r\n|_c1\n"
+    
+    child3 = TreeNode('c3')
+    child1.addChild(child3)
+    assert f"{node}" == "r\n|_c1\n  |_c3\n"
+
+    print(node)
+    print("test_append_node PASS")
 
 test_create_tree()
+test_append_node()
+
