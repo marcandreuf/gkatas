7a253f3e9f419830b59f98160eabb0c544896111
Marc Andreu <marcandreuf@gmail.com>
2022-11-18 23:40:11 +0900

    Implemented all insert conditions of avl tree with TDD.

--- trees/avl_tree.py
+++ trees/avl_tree.py
@@ -79,47 +79,56 @@ class AvlNode:
                     self.right = self.right.insert(value)
                 else:
                     self.right = AvlNode(value)
-            lh = self.left.height + 1 if self.left else 0
-            rh = self.right.height + 1 if self.right else 0
-            self.height = max(lh, rh)
+            self.height = max(AvlNode.calc_height(self))
             print(f"on node {self.data} before rebalance: {self.prt_hl_order()}")
-            bal_self = self._rebalance_node(self)
+            bal_self = AvlNode._rebalance_node(self)
             print(f"on node {bal_self.data} after rebalance: {bal_self.prt_hl_order()}")
             return bal_self
-            
+
         
-    def _rebalance_node(self, node): 
-        lh = 0 if node.left == None else node.left.height+1
-        rh = 0 if node.right == None else node.right.height+1
+    def _rebalance_node(node): 
+        lh, rh = AvlNode.calc_height(node)
         print(f"Rebalance node: {node.data} lh: {lh}, rh: {rh}")
         if abs(lh-rh) > 1:
             print(f"node {node.data} is not balanced")
             if node.left and node.left.left:
-                node = self._right_rotation(node)
+                print("left left condition");
+                return AvlNode._right_rotation(node)
             if node.left and node.left.right:
-                node = self._left_rotation(node)
+                print("left right condition")
+                node.left = AvlNode._left_rotation(node.left)
+                return AvlNode._right_rotation(node)
+            if node.right and node.right.right:
+                print("right right condition")
+                return AvlNode._left_rotation(node)
+            if node.right and node.right.left:
+                print("right left condition")
+                node.right = AvlNode._right_rotation(node.right)
+                return AvlNode._left_rotation(node)
         return node
         
 
-    def _left_rotation(self, disb_node): #left right condition
-        print(f"left {disb_node.left.data}")
-        print(f"left.right {disb_node.left.right.data}")
+    def _left_rotation(node):
+        new_root = node.right
+        node.right = node.right.left
+        new_root.left = node
+        node.height = max(AvlNode.calc_height(node))
+        new_root.height = max(AvlNode.calc_height(new_root))                
+        return new_root 
     
-    def _right_rotation(self, disb_node): #left left condition
-        print(f"left {disb_node.left.data}")
-        print(f"left.left {disb_node.left.left.data}")
-        new_root = disb_node.left
-        disb_node.left = disb_node.left.right
-        new_root.right = disb_node
-        lh = disb_node.left.height + 1 if disb_node.left else 0
-        rh = disb_node.right.height + 1 if disb_node.right else 0
-        disb_node.height = max(lh, rh)
-        lh = new_root.left.height + 1 if new_root.left else 0
-        rh = new_root.right.height + 1 if new_root.right else 0
-        new_root.height = max(lh, rh)                
+    def _right_rotation(node):
+        new_root = node.left
+        node.left = node.left.right
+        new_root.right = node
+        node.height = max(AvlNode.calc_height(node))
+        new_root.height = max(AvlNode.calc_height(new_root))                
         return new_root
 
-
+    def calc_height(node):
+        lh = node.left.height + 1 if node.left else 0
+        rh = node.right.height + 1 if node.right else 0
+        return lh,rh
+        
 
 def _sample_avl():
     avlt = AvlNode(30)
@@ -184,45 +193,56 @@ def test_insert_to_empty_tree():
 
 
 def test_insert_rebalance_left_left_condition():
-    avlt = AvlNode(None)
-    items = [30,25,35,20,10]
-    for i in items:
-        avlt = avlt.insert(i)
-    print(avlt.prt_hl_order())
+    avlt = _avl_tree([30,25,35,20,10])
     assert f"{avlt.prt_hl_order()}" == "['30(2)', '20(1)', '35(0)', '10(0)', '25(0)']"
-    avlt = AvlNode(None)
-    items = [70,50,90,30,60,20]
-    for i in items:
-        avlt = avlt.insert(i)
-    print(avlt.prt_hl_order())
+    
+    avlt = _avl_tree([70,50,90,30,60,20])
     assert f"{avlt.prt_hl_order()}" == "['50(2)', '30(1)', '70(1)', '20(0)', '60(0)', '90(0)']"
-    avlt = AvlNode(None)
-    items = [70,50,90,30,60,80,100,20,10]
-    for i in items:
-        avlt = avlt.insert(i)
-    print(avlt.prt_hl_order())
+    
+    avlt = _avl_tree([70,50,90,30,60,80,100,20,10])
     assert f"{avlt.prt_hl_order()}" == "['70(3)', '50(2)', '90(1)', '20(1)', '60(0)', '80(0)', '100(0)', '10(0)', '30(0)']"
     print("test_insert_rebalance_left_left_condition PASS")
 
 def test_insert_rebalance_left_right_condition():
-    avlt = AvlNode(None)
-    items = [70,50,90,30,60,80,100,20,25]
-    for i in items:
-        avlt = avlt.insert(i)
-    print(avlt.prt_hl_order())
+    avlt = _avl_tree([70,50,90,30,60,80,100,20,25])
     assert f"{avlt.prt_hl_order()}" == "['70(3)', '50(2)', '90(1)', '25(1)', '60(0)', '80(0)', '100(0)', '20(0)', '30(0)']"
+    
+    avlt = _avl_tree([30,10,20])
+    assert f"{avlt.prt_hl_order()}" == "['20(1)', '10(0)', '30(0)']"    
     print("test_insert_rebalance_left_left_condition PASS")
 
 
-def _sample_left_right():
-    avlt = AvlNode(None)
-    items = [70,50,90,30,60,80,100,20,25]
-    for i in items:
-        avlt.insert(i)
-    return avlt
-
-
+def test_insert_rebalance_right_right_condition():
+    avlt = _avl_tree([50,40,60,65,70])
+    assert f"{avlt.prt_hl_order()}" == "['50(2)', '40(0)', '65(1)', '60(0)', '70(0)']"
     
+    avlt = _avl_tree([50,40,65,60,70,75])
+    assert f"{avlt.prt_hl_order()}" == "['65(2)', '50(1)', '70(1)', '40(0)', '60(0)', '75(0)']"
+    
+    avlt = _avl_tree([30,40,50])
+    assert f"{avlt.prt_hl_order()}" == "['40(1)', '30(0)', '50(0)']"
+    print("test_insert_rebalance_right_right_condition PASS")
+
+def test_insert_rebalance_right_left_condition():
+    avlt = _avl_tree([50,40,60,70,65])
+    assert f"{avlt.prt_hl_order()}" == "['50(2)', '40(0)', '65(1)', '60(0)', '70(0)']"
+    
+    avlt = _avl_tree([30,40,35])
+    assert f"{avlt.prt_hl_order()}" == "['35(1)', '30(0)', '40(0)']"
+    print("test_insert_right_left_condition PASS")
+
+
+def test_insert_all_conditions():
+    avlt = _avl_tree([30,25,35,20,15,5,10,50,60,70,65])
+    assert f"{avlt.prt_hl_order()}" == "['20(3)', '10(1)', '50(2)', '5(0)', '15(0)', '30(1)', '65(1)', '25(0)', '35(0)', '60(0)', '70(0)']"
+    print("test_insert_all_conditions PASS")
+
+def _avl_tree(items):
+    avlt = AvlNode(None)
+    for i in items:
+        avlt = avlt.insert(i)
+    print(avlt.prt_hl_order())
+    return avlt
 
 test_create_avl_tree()
 test_pre_order_trav()
@@ -232,8 +252,9 @@ test_level_order_trav()
 test_insert_to_empty_tree()
 test_insert_rebalance_left_left_condition()
 test_insert_rebalance_left_right_condition()
-
-
+test_insert_rebalance_right_right_condition()
+test_insert_rebalance_right_left_condition()
+test_insert_all_conditions()
 
 
 # Getting the disbalanced node was a wrong path. It is much simpler to keep the tree balanced while recursively inserting new values. The goal is that the insert method garanties that the tree keeps balanced so it does not make sense to have insert and rotate methods. It is not good for performance and increments complexity.
@@ -323,6 +344,13 @@ test_insert_rebalance_left_right_condition()
 #    print(f"db2.data {db2.data}")
 #    print("test_get_deepest_disbalanced PENDING")
 
+#def _sample_left_right():
+#    avlt = AvlNode(None)
+#    items = [70,50,90,30,60,80,100,20,25]
+#    for i in items:
+#        avlt.insert(i)
+#    return avlt
+#
 #test_getDisbalanced_node()
 #test_rebalance_left_left()
 #test_rebalance_left_right()
