cdd7825fa43c9b13284a426e7f0e4d5eb900386e
Marc Andreu <marcandreuf@gmail.com>
2022-11-24 12:49:51 +0900

    Implemented pending delte and search tests for Tire with TDD.

--- trees/trie.py
+++ trees/trie.py
@@ -26,6 +26,55 @@ def test_insert():
     #assert t.words = ['APP', 'API', 'APIS']
     print("test_insert PENDING")
 
+def test_search():
+    t = Trie()
+    #t.insert('API')
+    #assert t.search('API')
+    print("test_search PENDING")
+
+def test_delete_part_prefix():
+    t = Trie()
+    #t.insert('API')
+    #t.insert('APPLE')
+    #t.delete('API')
+    #assert t.search('APPLE')
+    #assert t.search('API') == False
+    print("test_delete_part_prefix PENDING")
+
+def test_delete_full_prefix():
+    t = Trie()
+    #t.insert('APPLE')
+    #t.insert('API')
+    #t.insert('APIS')
+    #t.delete('API')
+    #assert t.search('API') == False 
+    #assert t.search('APPLE')
+    #assert t.search('APIS')
+    print("test_delete_full_prefix PENDING")
+
+def test_delete_long_word_on_top_of_other_word():
+    t = Trie()
+    #t.insert('APIS')
+    #t.insert('AP')
+    #t.delete('APIS')
+    #assert t.search('APIS') == False
+    #assert t.search('AP')
+    print("test_delete_long_word_on_top_of_other_word PENDING")
+
+def test_delte_word_with_no_dependencies():
+    t = Trie()
+    #t.insert('APIS')
+    #t.insert('K')
+    #t.delete('K')
+    #assert t.search('K') == False
+    #assert t.search('APIS')
+    print("test_delte_word_with_no_dependencies PENDING")
+
+
 test_create_Trie()
 test_insert()
-
+test_search()
+test_delete_part_prefix()
+test_delete_full_prefix()
+test_delete_long_word_on_top_of_other_word()
+test_delte_word_with_no_dependencies()
