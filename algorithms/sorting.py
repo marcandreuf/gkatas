fadf842e450347a32526c845a3b6ee530ba6788d
Marc Andreu <marcandreuf@gmail.com>
2022-12-05 12:45:29 +0900

    Implemented insert sort and prep test for bucket sort with TDD.

--- algorithms/sorting.py
+++ algorithms/sorting.py
@@ -17,13 +17,24 @@ def selection_sort(list):
         list[s], list[m] = list[m], list[s]
         #print(list)
 
+def insertion_sort(list):
+    for i in range(len(list)):
+        key = list[i]
+        j = i-1
+        while j>=0 and key < list[j]:
+            list[j+1] = list[j]
+            j -= 1
+        list[j+1] = key
+
+def bucket_sort():
+    pass
+
+
 def test_buble_sort():
     l = [3,5,2,7,9,3,4,6,7,5]
     buble_sort(l)
-    print(l)
     assert l == [2, 3, 3, 4, 5, 5, 6, 7, 7, 9]
     print("test_buble_sort PASS")
-
 
 def test_selection_sort():
     l = [3,5,2,7,9,3,4,6,7,5]
@@ -32,8 +43,24 @@ def test_selection_sort():
     assert l == [2, 3, 3, 4, 5, 5, 6, 7, 7, 9]
     print("test_selection_sort PASS")
 
+def test_insertion_sort():
+    l = [3,5,2,7,9,3,4,6,7,5]
+    insertion_sort(l)
+    print(l)
+    assert l == [2, 3, 3, 4, 5, 5, 6, 7, 7, 9]
+    print("test_insertion_sort PASS")
+
+def test_bucket_sort():
+    #l = [3,5,2,7,9,3,4,6,7,5]
+    #bucket_sort(l)
+    #print(l)
+    #assert l == [2, 3, 3, 4, 5, 5, 6, 7, 7, 9]
+    print("test_bucket_sort PENDING")
+
+
 test_buble_sort()
 test_selection_sort()
-
+test_insertion_sort()
+test_bucket_sort()
 
 
