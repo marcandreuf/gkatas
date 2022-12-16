41ffa75bc2dcd2ab51bb06efa546dac88efd2fd1
Marc Andreu <marcandreuf@gmail.com>
2022-12-16 12:26:49 +0900

    Reedit todos for graphs implementations with TDD.

--- graphs/graphs.py
+++ graphs/graphs.py
@@ -1,9 +1,18 @@
 
+"""
+ TODO. 
 
-# TODO. Implement a class for each type of graph.
-#   Implement add, remove and print methods
-#   Extra bonus implement search methods
-# Split in different files, one per type of graph.
+ * Implement two classes for each type of graph. 
+    One with adjacent matrix and another with adjancet list
+
+ * For each class implement add vertice, add edge, 
+    remove edge, remove vertice and print graph representation methods
+    
+ * Extra bonus implement search methods. 
+    Research search graph algorithms theory and examples.
+    
+ * Split in different files, one per type of graph.
+"""
 
 def test_unweighted_undirected():
     print("test_unweighted_undirected PENDING")
