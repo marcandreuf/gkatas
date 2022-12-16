595b14ea8d39d82ae6c0525b32c1f29693f56b49
Marc Andreu <marcandreuf@gmail.com>
2022-12-16 12:07:45 +0900

    Prepared initial graph test set with TDD.

---
+++ graphs/graphs.py
@@ -0,0 +1,32 @@
+
+
+# TODO. Implement a class for each type of graph.
+# Split in different files, one per type of graph.
+
+def test_unweighted_undirected():
+    print("test_unweighted_undirected PENDING")
+
+def test_unweighted_directed():
+    print("test_unweighted_directed PENDING")
+
+def test_positive_weighted_undirected():
+    print("test_positive_weighted_undirected PENDING")
+
+def test_positive_weighted_directed():
+    print("test_positive_weighted_directed PENDING")
+
+def test_negative_weighted_undirected():
+    print("test_negative_weighted_undirected PENDING")
+
+def test_negative_weighted_directed():
+    print("test_negative_weighted_directed PENDING")
+
+
+test_unweighted_undirected()
+test_unweighted_directed()
+test_positive_weighted_undirected()
+test_positive_weighted_directed()
+test_negative_weighted_undirected()
+test_negative_weighted_directed()
+
+
