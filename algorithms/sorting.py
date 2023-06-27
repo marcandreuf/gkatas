3482f4aedec33c0c92064298f0bc2dc97dfe1b1b
Marc Andreu <marcandreuf@gmail.com>
2023-06-27 12:05:01 +0900

    Tried bubble sort custome solution and started bucket sort.

--- algorithms/sorting.py
+++ algorithms/sorting.py
@@ -1,10 +1,30 @@
-
+import math
 
 def buble_sort(list):
+    count = 0
     for i in range(len(list)-1):
         for j in range(len(list)-i-1):
             if list[j] > list[j+1]:
-                list[j], list[j+1] = list[j+1], list[j]
+                swap(list, j, j+1)
+                count = count + 1 
+    print(f"buble_sort count {count}")
+
+def buble_sort_optim(list):
+    count = 0
+    ordered = False
+    while not ordered:
+        ordered = True
+        for i in range(len(list)-1):
+            if list[i] > list[i+1]:
+                swap(list, i, i+1)
+                count = count + 1
+                ordered = False
+    print(f"buble_sort_optim count {count}")
+                     
+
+def swap(list, e1, e2):
+    list[e1], list[e2] = list[e2], list[e1]
+    
 
 
 def selection_sort(list):
@@ -14,8 +34,8 @@ def selection_sort(list):
         for i in range(s+1,len(list)):
             if list[i] < list[m]:
                 m = i
-        list[s], list[m] = list[m], list[s]
-        #print(list)
+        swap(list, s, m)
+
 
 def insertion_sort(list):
     for i in range(len(list)):
@@ -26,8 +46,24 @@ def insertion_sort(list):
             j -= 1
         list[j+1] = key
 
-def bucket_sort():
-    pass
+def bucket_sort(list):
+    num_buckets = round(math.sqrt(len(list)))
+    num_elms_bucket = round(len(list) / num_buckets)
+    buckets = [[0 for i in range(num_elms_bucket)] for i in range(num_buckets)]
+    max_value = max(list)
+    print(f"max: {max_value}")
+    
+    bucket_num = 0
+    for i in range(len(list)-1):
+        bucket_num = round(list[i] * num_buckets / max_value)
+        print (f"add {list[i]} to bucket ")
+        buckets[bucket_num][i] = list[i]
+    
+    for a in buckets:
+        ln = ""
+        for i in a:
+            ln += str(i) + " "
+        print(ln)
 
 def merge_sort():
     pass
@@ -36,32 +72,36 @@ def quick_sort():
     pass
 
 def test_buble_sort():
-    l = [3,5,2,7,9,3,4,6,7,5]
+    l = [1,2,3,5,2,7,9,3,4,6,7,5]
     buble_sort(l)
-    assert l == [2, 3, 3, 4, 5, 5, 6, 7, 7, 9]
+    assert l == [1, 2, 2, 3, 3, 4, 5, 5, 6, 7, 7, 9]
     print("test_buble_sort PASS")
+
+def test_buble_sort_optim():
+    l = [1,2,3,5,2,7,9,3,4,6,7,5]
+    buble_sort_optim(l)
+    assert l == [1, 2, 2, 3, 3, 4, 5, 5, 6, 7, 7, 9]
+    print("buble_sort_optim PASS")    
 
 def test_selection_sort():
     l = [3,5,2,7,9,3,4,6,7,5]
-    selection_sort(l)
-    print(l)
+    selection_sort(l)    
     assert l == [2, 3, 3, 4, 5, 5, 6, 7, 7, 9]
     print("test_selection_sort PASS")
 
 def test_insertion_sort():
     l = [3,5,2,7,9,3,4,6,7,5]
     insertion_sort(l)
-    print(l)
     assert l == [2, 3, 3, 4, 5, 5, 6, 7, 7, 9]
     print("test_insertion_sort PASS")
 
-# TODO test_bucket_sort
+
 def test_bucket_sort():    
-    #l = [3,5,2,7,9,3,4,6,7,5]
-    #bucket_sort(l)
-    #print(l)
-    #assert l == [2, 3, 3, 4, 5, 5, 6, 7, 7, 9]
-    print("test_bucket_sort PENDING")
+    l = [3,5,2,7,9,3,4,6,7,5]
+    bucket_sort(l)
+    print(l)
+    assert l == [2, 3, 3, 4, 5, 5, 6, 7, 7, 9]
+    print("test_bucket_sort PASS")
 
 # TODO test_merge_sort
 def test_merge_sort():
@@ -88,11 +128,12 @@ def test_heap_sort():
     print("test_heap_sort PENDING")
 
 
-test_buble_sort()
-test_selection_sort()
-test_insertion_sort()
+#test_buble_sort()
+#test_buble_sort_optim()
+#test_selection_sort()
+#test_insertion_sort()
 test_bucket_sort()
-test_merge_sort()
-test_quick_sort()
-test_heap_sort()
+#test_merge_sort()
+#test_quick_sort()
+#test_heap_sort()
 
